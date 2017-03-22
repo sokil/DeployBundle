ae5df4f731c29394c849f68b6f6da9405f0b5984
Dmytro Sokil <dmytro.sokil@gmail.com>
2017-03-22 22:49:13 +0200

    composer

---
+++ tests/Task/SyncTaskTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Sokil\DeployBundle\Task;
+
+use Sokil\DeployBundle\AbstractTestCase;
+
+class SyncTaskTest extends AbstractTestCase
+{
+    public function testRun()
+    {
+
+    }
+}
